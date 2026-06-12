15c66ecf5c0ab1505a1298527ad0c064b699b4f6
Sam Tarling <sgtarling@icloud.com>
2026-06-12 14:55:24 +1000

    fix: serve privacy and terms via serverless functions

---
+++ api/terms.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const html = fs.readFileSync(path.join(__dirname, '../public/terms.html'), 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(html);
+};
